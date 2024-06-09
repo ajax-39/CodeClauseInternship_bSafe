8fe222b0416b09a04e9325454b5f663ec5cdeb23
Atharv Jagzap <atharvjagzap@gmail.com>
2024-06-09 10:53:00 +0530

    change launcher icon and name

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    Timer(const Duration(seconds: 3), () {
+    Timer(const Duration(seconds: 2), () {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(
           builder: (ctx) => const HomeScreen(),
